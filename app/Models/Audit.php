addbfb920dc4762fbf5540988bf0701dbe6d5725
Harold Hernandez <martinezharold059@gmail.com>
2026-03-12 00:27:03 -0600

    feat(audit): add fillable in model

--- app/Models/Audit.php
+++ app/Models/Audit.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Audit extends Model
 {
-  
+  protected $fillable = [
+    'user_id',
+    'affected_module',
+    'action_performed',
+    'date_time',
+    'detail',
+  ];
 }
