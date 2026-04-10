9af0b3a4320c459a55e48f7464ce3503a80b4a6e
Miguel Hinojos <taxzo1126@gmail.com>
2026-04-09 18:07:11 -0600

    feat(orders): add automatic order cancellation command

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,13 @@
 <?php
 
+
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('app:cancel-expired-orders-comand')->everyFourHours($minutes = 0); //ya en proyecto y servidor real
+//Schedule::command('app:cancel-expired-orders-comand')->everyMinute(); //para pruebas en servidor local
